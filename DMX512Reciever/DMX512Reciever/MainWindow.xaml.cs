599a9d232c60d7ab38f5a58794e4fd26faf37a81
nitteru <nitteru@gmail.com>
2017-07-25 22:10:56 +0900

    * New solution * Modifying XAML

---
+++ DMX512Reciever/DMX512Reciever/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DMX512Reciever
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // シリアルポートオブジェクト
+        private SerialPort _serialPort;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
