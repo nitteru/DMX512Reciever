b199857976482b4fa15c47055d9cc1569cbcfb75
西輝之 <nishi@elm.co.jp>
2017-07-26 12:54:13 +0900

    * 画面周り修正    * Scrollviewを使ってみる * 受信周りの実装    * COMポートの列挙、Comboboxへの表示    * MVVMについて調査中

--- DMX512Reciever/DMX512Reciever/MainWindow.xaml.cs
+++ DMX512Reciever/DMX512Reciever/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO.Ports;
 using System.Linq;
 using System.Text;
@@ -16,6 +17,10 @@ using System.Windows.Shapes;
 
 namespace DMX512Reciever
 {
+    public class ComPort
+    {
+        public string portName { get; set; }
+    }
     /// <summary>
     /// MainWindow.xaml の相互作用ロジック
     /// </summary>
@@ -24,9 +29,49 @@ namespace DMX512Reciever
         // シリアルポートオブジェクト
         private SerialPort _serialPort;
 
+        /* XAMLで定義したコントロールのリスト
+         *
+         * @note
+         *   - 本来ならVisualTree等を使って操作するべきところ
+         *   - XAMLで記述順に参照できる
+         */
+        //XAMLで定義したラベルのリスト
+        List<Label> _labels = new List<Label>();
+        // XAMLで定義した矩形のリスト
+        List<Rectangle> _rectangles = new List<Rectangle>();
+
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        private void Label_Loaded(object sender, RoutedEventArgs e)
+        {
+            _labels.Add((Label)sender);
+        }
+
+        private void Rectangle_Loaded(object sender, RoutedEventArgs e)
+        {
+            _rectangles.Add((Rectangle)sender);
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private ObservableCollection<ComPort> GetComPorts()
+        {
+            var results = new ObservableCollection<ComPort>();
+            string[] ports = SerialPort.GetPortNames();
+
+            foreach (var m in ports)
+                {
+                    results.Add(new ComPort()
+                    {
+                        portName = (string)m,
+                    });
+                }
+            return results;
     }
 }
